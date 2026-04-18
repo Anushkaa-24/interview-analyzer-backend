3c6dd34ccff1eccaa3ccc4d925e614510803f496
Anushkaa-24 <anusshkaaaa24@gmail.com>
2026-04-18 18:29:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI(title="Interview Analyzer API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
